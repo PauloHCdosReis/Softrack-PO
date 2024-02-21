330adfc61b533c878d745fe2ed79fcf01394c161
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-02-21 16:38:35 -0300

    Create resposta.ts

---
+++ resposta.ts
@@ -0,0 +1,66 @@
+const kask = {
+  perguntas: [
+    {
+      critica: true,
+      descricao:
+        "O botão de emergência é um componente crítico em uma empilhadeira, projetado para interromper imediatamente todas as operações em caso de emergência. Ele é essencial para garantir a segurança do operador, dos pedestres e do ambiente de trabalho. Ao ser acionado, o botão de emergência deve interromper todas as funções da empilhadeira, incluindo movimento, elevação e qualquer outra operação ativa. A funcionalidade adequada deste botão é vital para responder rapidamente a situações de risco, minimizando assim o potencial de danos pessoais ou materiais. Portanto, durante a inspeção da empilhadeira, é fundamental verificar se o botão de emergência está posicionado corretamente, acessível ao operador e, o mais importante, se está funcionando conforme o esperado. Qualquer sinal de mau funcionamento ou desgaste deve ser tratado imediatamente para garantir um ambiente de trabalho seguro e eficiente.",
+      filhas: [Array],
+      idPergunta: 1,
+      ordem: 1,
+      pergunta: "Botão de emergência está funcionando?",
+      resposta: null,
+    },
+    {
+      critica: false,
+      descricao:
+        "Verifique se a bateria não possui fios expostos ou danificados que possam causar curtos-circuitos ou riscos elétricos.",
+      filhas: [Array],
+      idPergunta: 2,
+      ordem: 2,
+      pergunta: "Bateria sem fio exposto?",
+      resposta: null,
+    },
+    {
+      critica: false,
+      descricao:
+        "Certifique-se de que os avisos de advertência e capacidade estejam visíveis e legíveis, fornecendo informações importantes sobre o uso e as capacidades operacionais da empilhadeira.",
+      filhas: [Array],
+      idPergunta: 4,
+      ordem: 4,
+      pergunta: "Avisos de advertência e capacidade existe?",
+      resposta: null,
+    },
+    {
+      critica: true,
+      descricao:
+        "Examine os pneus e rodas para garantir que não haja rachaduras, cortes ou desgaste excessivo que possam comprometer a segurança ou a capacidade de manobra da empilhadeira.",
+      filhas: [Array],
+      idPergunta: 5,
+      ordem: 5,
+      pergunta: "Pneus e rodas sem rachaduras e desgaste excessivo?",
+      resposta: null,
+    },
+    {
+      critica: false,
+      descricao:
+        "Verifique se a empilhadeira está limpa e livre de sujeira, detritos ou qualquer outra obstrução que possa interferir em seu funcionamento seguro e eficiente.",
+      filhas: [Array],
+      idPergunta: 6,
+      ordem: 6,
+      pergunta: "Equipamento está limpo?",
+      resposta: null,
+    },
+    {
+      critica: true,
+      descricao:
+        "O botão de emergência é um componente crítico em uma empilhadeira, projetado para interromper imediatamente todas as operações em caso de emergência. Ele é essencial para garantir a segurança do operador, dos pedestres e do ambiente de trabalho. Ao ser acionado, o botão de emergência deve interromper todas as funções da empilhadeira, incluindo movimento, elevação e qualquer outra operação ativa. A funcionalidade adequada deste botão é vital para responder rapidamente a situações de risco, minimizando assim o potencial de danos pessoais ou materiais. Portanto, durante a inspeção da empilhadeira, é fundamental verificar se o botão de emergência está posicionado corretamente, acessível ao operador e, o mais importante, se está funcionando conforme o esperado. Qualquer sinal de mau funcionamento ou desgaste deve ser tratado imediatamente para garantir um ambiente de trabalho seguro e eficiente.",
+      filhas: [Array],
+      idPergunta: 1,
+      ordem: 1,
+      pergunta: "Botão de emergência está funcionando?",
+      resposta: true,
+    },
+  ],
+  tempoMaximo: 180,
+  tempoMinimo: 10,
+};
